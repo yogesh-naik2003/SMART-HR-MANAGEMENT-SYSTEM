dc9d705026627251d6f46e7909bcb7f147629ba3
yogesh-naik2003 <yogeshbgscet@gmail.com>
2026-07-19 16:48:55 +0530

    Fix 401 error: auto logout on 401 and respect NEXT_PUBLIC_API_URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001/api"
 });
 
 api.interceptors.request.use((config) => {
@@ -13,5 +13,18 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      Cookies.remove("token");
+      if (typeof window !== "undefined") {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
